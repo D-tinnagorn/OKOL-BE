ff9788fb914f3ad366bd6d4312f1b3f094f4f7a8
korndev1 <tinnakorn@onelifegroupholding.com>
2026-01-06 19:26:23 +0700

    commit

--- src/todo/todo.gateway.ts
+++ src/todo/todo.gateway.ts
@@ -2,14 +2,24 @@ import {
   WebSocketGateway,
   WebSocketServer,
 } from '@nestjs/websockets';
+import { Todo } from '@prisma/client';
 import { Server } from 'socket.io';
 
-@WebSocketGateway({ cors: true })
+// TodoGateway.ts
+@WebSocketGateway()
 export class TodoGateway {
-  @WebSocketServer()
-  server: Server;
+  @WebSocketServer() server: Server;
 
-  emitTodoCreated(todo: any) {
-    this.server.emit('todo_created', todo);
+  afterCreateTodo(todo: Todo) {
+    this.server.emit('todo_updated', todo);
+  }
+
+  afterUpdateTodo(todo: Todo) {
+    this.server.emit('todo_updated', todo);
+  }
+
+  afterDeleteTodo(id: string) {
+    this.server.emit('todo_updated', { id });
   }
 }
+
